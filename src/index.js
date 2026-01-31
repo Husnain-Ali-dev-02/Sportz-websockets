ef94e4ad324dec47d54e1ea4809c55c5f7e78a8b
Husnain Ali <husnain.dev.009@gmail.com>
2026-01-31 23:11:33 +0500

    feat: add WebSocket server integration and match broadcasting functionality

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import express from 'express';
 import { matchRouter } from './routes/matches.js';
+import {attachWebSocketServer} from "./ws/server.js";
+import http from 'http';
 
+
+
+const PORT = Number(process.env.PORT || 8000);
+const HOST = (process.env.HOST || '0.0.0.0');
 const app = express();
-const PORT = 8000;
+const server = http.createServer(app);
 
 app.use (express.json());
+
 
 
 app.get('/', (req, res) => {
@@ -12,6 +19,13 @@ res.send('Hello from Express server');
 })
 app.use('/matches', matchRouter);
 
-app.listen(PORT, () => {
-console.log(`Server is running on http://localhost:${PORT}`);
+const {broadcastMatchCreated} = attachWebSocketServer(server);
+app.locals.broadcastMatchCreated = broadcastMatchCreated;
+
+
+server.listen(PORT, HOST, () => {
+    const baseUrl = HOST === '0.0.0.0' ? `http://localhost:${PORT}` : `http://${HOST}:${PORT}`;
+
+    console.log(`Server is running on ${baseUrl}`);
+    console.log(`WebSocket Server is running on ${baseUrl.replace('http', 'ws')}/ws`);
 });
